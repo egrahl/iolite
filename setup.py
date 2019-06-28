0a2d1e2187eb12889a50e446ae5b28864d9646fe
Grahlert <gwx73773@ws358.diamond.ac.uk>
2019-06-28 17:02:08 +0100

    copied the code for the setup script

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
-# some text for the beginning
+from setuptools import setup
+
+def main():
+    """
+    setup the package
+
+    """
+    setup(
+        packages=["src/iolite"],
+        install_requires=[],
+        setup_requires["pytest-runner"],
+        test-require={"pytest", "pytest-cov", "mock"],
+        test_suite="tests",
+        extras_require={build_sphinx": ["sphinx", "sphinx_rtd_theme"]},
+    )
+
+
+if __name__ == "__main__":
+    main()
