c53a517164a1dc25d25fa2942dc7baf210379e16
Grahlert <gwx73773@ws358.diamond.ac.uk>
2019-07-11 17:19:45 +0100

    added ratio calculation

--- src/iolite/ice_rings.py
+++ src/iolite/ice_rings.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from timeit import default_timer as timer
 
+
+def calculateRatio (res,intensity,startRes,endRes, boundary):
+    indexstart =res.index(startRes)
+    indexend = res.index(endRes)
+    indexboundary = res.index(boundary)
+    intensity_peak = (sum(intensity,indexstart)-sum(intensity,indexend+1))/(indexend-indexstart)
+    intensity_bg = (sum(intensity,indexend)-sum(intensity,indexboundary+1))/(indexboundary-indexend)
+    return (intensity_peak/intensity_bg)
+
 start=timer()
 
 
@@ -64,11 +73,11 @@ for i in range(len(resolution_data)):
 end=timer()
 print('time used:', end-start)
 
-resolution_data= np.array(resolution_data)
-mean_int = np.array(mean_int)
+#resolution_data= np.array(resolution_data)
+#mean_int = np.array(mean_int)
 
-resMInt = np.stack((resolution_data,mean_int), axis =-1)
-print(resMInt)
+#resMInt = np.stack((resolution_data,mean_int), axis =-1)
+#print(resMInt)
 
 plt.plot(resolution_data,mean_int)
 plt.xlim(4.0, 1.3)                 #invert the x axis
@@ -80,11 +89,20 @@ plt.show()
 
 """
 the ice-rings have to be detected by the program
---> get intensity at the common ice-ring resolutions (3.65 A, 2.25 A and 1.92 A) and calculate the difference in intensity 
+--> get intensity at the common ice-ring resolutions (3.67 A, 2.25 A and 1.92 A) and calculate the difference in intensity 
 compared to a 'slightly' (needs to be defined) lower or higher resolution
 classify this difference as either detection of an ice-ring or not
 one could also compare the absolute values of the derivatives (should be a significantly higher value at the ice-ring?)
 
 
 """
+# calculate the intensity ratios; SOMETHING IS WRONG HERE!!!
 
+ratio1 = calculateRatio(resolution_data, mean_int,3.65,3.69,3.90)
+ratio2 = calculateRatio(resolution_data, mean_int,2.24,2.25,2.45)
+ratio3 = calculateRatio(resolution_data, mean_int,1.91,1.93,2.00)
+
+print(ratio1, ratio2, ratio3)
+
+sumratios = ratio1 + ratio2 + ratio3
+print(sumratios)
