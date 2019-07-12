3062f2497c59f5f30ae8ded1bfd6ce31547ba9ee
Grahlert <gwx73773@ws358.diamond.ac.uk>
2019-07-12 17:21:09 +0100

    plot data from table.txt

--- src/iolite/ice_rings.py
+++ src/iolite/ice_rings.py
@@ -4,6 +4,15 @@ import numpy as np
 from timeit import default_timer as timer
 
 
+filein=open("table.txt","r")
+
+resolution_data = []
+intensity_data = []
+
+for line in filein.readlines():
+    tokens = line.split(",")
+    resolution_data.append(tokens[0])
+    intensity_data.append(tokens[1])
 
 
 
@@ -139,9 +148,9 @@ print('time used:', end-start)
 """
 
 #plot data
-plt.plot(resolution_data,mean_int)
-plt.xlim(max(resolution_data),min(resolution_data) )                 #invert the x axis
+plt.plot(resolution_data,intensity_data)
+#plt.xlim(max(resolution_data),min(resolution_data) )                 #invert the x axis
 plt.ylabel('Mean Intensity')
-plt.xlabel('Resolution (A)')
+plt.xlabel('Resolution')
 plt.title('Mean intensity vs resolution')
 plt.show()
