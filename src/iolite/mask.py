1c1e3ba4bf6b75852cd0ac7a9d4663186888cf95
Grahlert <gwx73773@ws358.diamond.ac.uk>
2019-07-18 17:14:51 +0100

    applied mask to image

--- src/iolite/mask.py
+++ src/iolite/mask.py
@@ -1,12 +1,19 @@
 from dials.array_family import flex
 from dxtbx.model.experiment_list import ExperimentListFactory
+from timeit import default_timer as timer
 import numpy as np
 
+
+def init_list_of_objects(size):
+    list_of_objects = list()
+    for i in range(0,size):
+        list_of_objects.append( list() ) #different object reference each time
+    return list_of_objects
 
 
 
 if __name__ == "__main__":
-
+    start = timer()
     experiments = ExperimentListFactory.from_json_file("datablock.json")
     assert len(experiments) == 1
 
@@ -14,50 +21,70 @@ if __name__ == "__main__":
     detector = experiments[0].detector
     imageset = experiments[0].imageset
     panel = detector[0]
-
-    print(len(imageset))
-    
   
     reflections = flex.reflection_table.from_pickle("strong.pickle")
     
-    #print(reflections[0])
 
     shoebox = reflections['shoebox']
    
-    mask_list = [np.ones((image_width, image_length))]*len(imageset)    #need to find the right expression for that 
+
+    #get dimensions of the image
+    y_dim = imageset.get_raw_data(0)[0].all()[0]   
+    x_dim = imageset.get_raw_data(0)[0].all()[1]     
 
     
+    #create list nested list of shoeboxes 
+    image_shoebox_l=init_list_of_objects(len(imageset))
+
     for sbox in shoebox:
         x0, x1, y0, y1, z0, z1 = sbox.bbox
-        #print(sbox.bbox) # ()
-        mask = sbox.mask # 3D array where 0 is background and 1 | 4 = 5 is foreground
-        mask_np = mask.as_numpy_array()
-        #print(mask_np)
         
-        for n in np.nditer(mask_np,op_flags = ['readwrite']):
-            if n ==5:
-                n[...] = False
-            else:
-                n[...] = True
-        print(mask_np)
-
-        for z in range(z0,z1):
-            for y in range(y0,y1):
-                for x in range(x0,x1):
-                    mask_list[z][y][x] = #the mask_np array has different dimensions, I need to get the right elements
+        for i in range(z0,z1):
+            image_shoebox_l[i].append(sbox)
         
     
-   
+    summed_data = None
+    summed_mask = None
+
+    # Read image
+    for n in range(len(imageset)):
+        mask_array = np.zeros((y_dim,x_dim), dtype=bool) 
+        data = imageset.get_raw_data(i)
+        mask = imageset.get_mask(i)
+
+        assert isinstance(data, tuple)
+        assert isinstance(mask, tuple)
+
+        for sbox in image_shoebox_l[n]:
+                
+                x0, x1, y0, y1, z0, z1 = sbox.bbox
+                mask_sp = sbox.mask # 3D array where 0 is background and 1 | 4 = 5 is foreground
+                mask_np = mask_sp.as_numpy_array()
+                mask_np_slice = mask_np[n-z0]
+                true_pixels = (mask_np_slice == 5)
+                mask_array[y0:y1,x0:x1] = np.logical_or(mask_array[y0:y1,x0:x1], true_pixels)
+
+        mask_array = ~mask_array
+        mask_array_flex=flex.bool(mask_array)
+        mask_strong_spots=tuple((mask_array_flex))
+        
+        if summed_data is None:
+            summed_mask = [mss and m for mss, m in zip(mask_strong_spots, mask)]
+            summed_data = data
+        else:
+            summed_data = [sd + d for sd, d in zip(summed_data, data)]
+            summed_mask = [sm and mss and m for sm, mss, m in zip(summed_mask,mask_strong_spots, mask)]
+        
+    summed_mask_np = summed_mask[0].as_numpy_array()
+    end = timer()
+    print('Time taken:', end-start)
 
 
-    """
-    for i in range(10):
-        coord = shoebox[i].bbox
-        mask = shoebox[i].mask 
-        mask_np = mask.as_numpy_array()
 
-        print(coord)
-        print(mask_np)
+    #show example mask
+    from matplotlib import pylab
+    pylab.imshow(summed_mask_np)
+    pylab.show()
+    
 
-    #print(shoebox_coord)
-    """
+
