122fae4255d88f3a2956dd87b65470150c20cfb0
Grahlert <gwx73773@ws358.diamond.ac.uk>
2019-07-24 15:45:14 +0100

    put sum data and mask in own function

--- src/iolite/radial_average_bg.py
+++ src/iolite/radial_average_bg.py
@@ -3,13 +3,10 @@ from dials.array_family import flex
 from dxtbx.model.experiment_list import ExperimentListFactory
 from timeit import default_timer as timer
 import numpy as np
-import logging
-
-logger = logging.getLogger(__name__)
 
 help_message = """
 
-This program masks the strong spots of an image, followed by averaging of the images and making a radial average over resolution shells
+This program masks the strong spots of an imageset, followed by averaging of the images and making a radial average over resolution shells.
 
 
 """
@@ -19,7 +16,11 @@ from libtbx.phil import parse
 
 phil_scope = parse(
     """
-
+  input {
+    filename_pickle = 'strong.pickle'
+        .type = str
+        .help = "The filename of the file containing information about strong spots."
+  }
   output {
     filename = 'table.txt'
       .type = str
@@ -46,16 +47,16 @@ phil_scope = parse(
 )
 
 
-class Script(object):
-    """ The integration program. """
+class Script:
+    '''This class masks the strong spots of an imageset, followed by averaging of the images and making a radial average over resolution shells.'''
 
     def __init__(self):
-        """Initialise the script."""
+        '''Initialise the script.'''
         from dials.util.options import OptionParser
         import libtbx.load_env
 
         # The script usage
-        usage = "usage: dev.dials.make_radial_average [options] models.expt"
+        usage = "usage: iolite.radial_average_bg [options] datablock.json"
 
         # Create the parser
         self.parser = OptionParser(
@@ -63,64 +64,33 @@ class Script(object):
         )
 
     def init_list_of_objects(self,size):
-        """ Create a list of empty lists of lenght size.""" 
+        ''' Create a list of empty lists of length size.
+        
+        :param int size: length of the nested list
+        :returns: list of length size of empty lists  
+        ''' 
+
         list_of_objects = list()
         for i in range(size):
             list_of_objects.append(list()) 
         return list_of_objects
 
+    def summed_data_mask(self,imageset,scan_range,shoebox):
+        '''Create numpy array of summed data and summed mask of an imageset,respectively, including masking of strong spots. 
 
-    def run(self):
-        """ Perform the integration. """
-        from dials.util.options import flatten_experiments
-        from dials.util import log
-        
-       
-        
-        #start timer
-        start=timer()
-
-        # Parse the command line
-        params, options = self.parser.parse_args(show_diff_phil=False)
-        experiments = ExperimentListFactory.from_json_file("datablock.json")
-        if len(experiments) == 0:
-            self.parser.print_help()
-            return
-
-        assert len(experiments) == 1
-        imageset = experiments[0].imageset
-        beam = experiments[0].beam
-        detector = experiments[0].detector
-
-        # Configure logging
-        log.config()
-
-        # Set the scan range
-        if params.scan_range is None:
-            scan_range = (0, len(imageset))
-           
-        else:
-            scan_range = params.scan_range
-            i0, i1 = scan_range
-           
-            if i0 < 0 or i1 > len(imageset):
-                raise RuntimeError("Scan range outside image range")
-            if i0 >= i1:
-                raise RuntimeError("Invalid scan range")
-    
-
-        reflections = flex.reflection_table.from_pickle("strong.pickle")
-        shoebox = reflections['shoebox']
-        
+        :param dxtbx_imageset_ext.ImageSweep imageset: imageset that contains the raw data
+        :param tuple scan_range: range of images in the imageset
+        :param list shoebox: list of all shoeboxes found in the imageset
+        :returns: numpy array summed_data and summed_mask
+        '''
     
         #get dimensions of the image
         y_dim = imageset.get_raw_data(0)[0].all()[0]   
         x_dim = imageset.get_raw_data(0)[0].all()[1]     
 
-        #create nested list of shoeboxes 
+        #create nested list of shoeboxes per image
         _,_,_,_,_,z1 = shoebox[-1].bbox
         image_shoebox_l=self.init_list_of_objects(z1)
-
         for sbox in shoebox:
             x0, x1, y0, y1, z0, z1 = sbox.bbox
             for i in range(z0,z1):
@@ -129,12 +99,8 @@ class Script(object):
         summed_data = None
         summed_mask = None
 
-      
-
         # Loop through images
         for n in range(*scan_range):   
-            logger.info("Reading image %d" % i)
-
             mask_array = np.zeros((y_dim,x_dim), dtype=bool) 
             data = tuple(i.as_numpy_array() for i in imageset.get_raw_data(n))[0]
             mask = tuple(m.as_numpy_array() for m in imageset.get_mask(n))[0]
@@ -161,6 +127,47 @@ class Script(object):
             else:
                 summed_data += temp
                 summed_mask += mask_combined
+        
+        return summed_data, summed_mask
+
+    def run(self):
+        ''' Perform the integration.'''
+        from dials.util.options import flatten_experiments
+        from dials.util import log
+        
+        #start timer
+        start=timer()
+
+        # Parse the command line
+        params, options = self.parser.parse_args(show_diff_phil=False)
+        experiments = flatten_experiments(params.input.experiments)   
+        if len(experiments) == 0:
+            self.parser.print_help()
+            return
+
+        assert len(experiments) == 1
+        imageset = experiments[0].imageset
+        beam = experiments[0].beam
+        detector = experiments[0].detector
+        reflections = flex.reflection_table.from_pickle(params.input.filename_pickle)
+        shoebox = reflections['shoebox']
+
+        
+        # Set the scan range
+        if params.scan_range is None:
+            scan_range = (0, len(imageset))
+           
+        else:
+            scan_range = params.scan_range
+            i0, i1 = scan_range
+           
+            if i0 < 0 or i1 > len(imageset):
+                raise RuntimeError("Scan range outside image range")
+            if i0 >= i1:
+                raise RuntimeError("Invalid scan range")
+    
+        #apply masks on data, sum up data and mask
+        summed_data, summed_mask = self.summed_data_mask(imageset, scan_range,shoebox)
 
         #calculate the average
         index = np.where(summed_mask > 0)
@@ -171,6 +178,11 @@ class Script(object):
         #filter out random high intensity pixels
         from scipy.signal import medfilt
         average = medfilt(average)
+
+        #save average as .png file
+        from matplotlib import pylab
+        pylab.imsave('average_data',average)
+        
 
         # Compute min and max and num
         if params.num_bins is None:
@@ -183,11 +195,7 @@ class Script(object):
             params.d_min = detector.get_max_resolution(beam.get_s0())
         vmax = (1.0 / params.d_min) ** 2
 
-        # Print some info
-        logger.info("Min 1/d^2: %f" % vmin)
-        logger.info("Max 1/d^2: %f" % vmax)
-        logger.info("Num bins:  %d" % num_bins)
-
+        
         # Compute the radial average
         from dials.algorithms.background import RadialAverage
 
@@ -196,29 +204,17 @@ class Script(object):
         mean = radial_average.mean()
         reso = radial_average.inv_d2()
 
-        # import matplotlib.pyplot as plt
-        # plt.plot(reso, mean)
-        # plt.ylabel('Mean Intensity')
-        # plt.xlabel('Resolution')
-        # plt.title('Mean intensity vs resolution')
-        # plt.show()
-
         #measure time taken
         end=timer()
         print('Time Taken:', end-start)
 
         #write output file
-        logger.info("Writing to %s" % params.output.filename)
         with open(params.output.filename, "w") as outfile:
             for r, m in zip(reso, mean):
                 outfile.write("%f, %f\n" % (r, m))
 
 
 if __name__ == "__main__":
-    from dials.util import halraiser
-
-    try:
         script = Script()
         script.run()
-    except Exception as e:
-        halraiser(e)
+    
