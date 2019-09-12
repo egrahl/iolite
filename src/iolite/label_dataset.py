fc06bb600677424744b741b10b29279f1065726a
Grahlert <gwx73773@ws358.diamond.ac.uk>
2019-09-12 15:46:31 +0100

    changed imports

--- src/iolite/label_dataset.py
+++ src/iolite/label_dataset.py
@@ -1,9 +1,9 @@
 import os
 from os.path import basename
 from prettytable import PrettyTable
-from classification.classify_sigma import SigmaClassifier
-from classification.classify_overlaps import OverlapClassifier
-from ice_ring.ice_rings import IceRingClassifier
+from src.iolite.classification.classify_sigma import SigmaClassifier
+from src.iolite.classification.classify_overlaps import OverlapClassifier
+from src.iolite.ice_ring.ice_rings import IceRingClassifier
 
 class DatasetLabeller:
     """This class labels datasets with regard to ice-rings, overlapping
